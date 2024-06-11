1fb0f4d7ca102a2ef55ebd6ee2e922b7cb794ed7
Andy <hinchliffe106@gmail.com>
2024-06-11 15:01:04 +0100

    apiKey

--- src/app/components/Openai.jsx
+++ src/app/components/Openai.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import OpenAI from "openai";
 
-const openai = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY  });
+const openai = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY, dangerouslyAllowBrowser: true  });
 
 // const freestyle = "";
 
